60abcd90f8a3de69cad05ddced23dfecaef7706b
devfromnyc <devfromnyc@gmail.com>
2023-08-27 17:35:57 -0400

    update: default port value

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@savr.xikai7q.mongodb.net/?retryWrites=true&w=majority`)
   .then(() => {
-    app.listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => {
     console.log(err);
